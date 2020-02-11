cc8886a9401d3946c5d67d0b1c8ca06d718b4036
Ewan Keith <ewan.keith@mudano.com>
2020-02-11 15:39:13 +0000

    Revert "nightscout transfer service now pulls params from ssm rather than env"
    This reverts commit 3b19218a31df6a90c529728adc1c677cb049e393.

--- nightscout-ingester/ingester.py
+++ nightscout-ingester/ingester.py
@@ -3,19 +3,10 @@ from ingester.jobs import nightscout_ingest_job
 from datetime import datetime
 import os
 import math
-import boto3
-
-# set up ssm client for config
-ssm = boto3.client('ssm', region_name='eu-west-1')
-
-
-def get_ssm_value(param_name):
-    return ssm.get_parameter(Name = param_name)['Parameter']['Value']
-
 
 if __name__ == '__main__':
 
-    ingest_interval_hours = float(get_ssm_value('openaps-ingest-interval'))
+    ingest_interval_hours = float(os.getenv('INGEST_INTERVAL_HOURS'))
     ingest_interval_seconds = int(math.ceil(ingest_interval_hours * 3600))
 
     app_scheduler.add_job(nightscout_ingest_job, 'interval', seconds=ingest_interval_seconds,
--- nightscout-ingester/ingester/scheduler.py
+++ nightscout-ingester/ingester/scheduler.py
@@ -4,21 +4,13 @@ from apscheduler.schedulers.background import BlockingScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor
 import os
-import boto3
-
-# set up ssm client for config
-ssm = boto3.client('ssm', region_name='eu-west-1')
 
 
-def get_ssm_value(param_name):
-    return ssm.get_parameter(Name = param_name)['Parameter']['Value']
-
-
-pg_host = get_ssm_value('openaps-postgres-host')
-pg_port = get_ssm_value('openaps-postgres-port')
-pg_db = get_ssm_value('aurora-db-name')
-pg_user = get_ssm_value('postgres-nightscout-user')
-pg_pass = get_ssm_value('postgres-nightscout-password')
+pg_host = os.environ['POSTGRES_HOST']
+pg_port = os.environ['POSTGRES_PORT']
+pg_db = os.environ['POSTGRES_DB']
+pg_user = os.environ['POSTGRES_USER']
+pg_pass = os.environ['POSTGRES_PASSWORD']
 postgres_connection_string = f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
 
 
