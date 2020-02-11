e8c8f5eb90846d5116748e95739d59f8b56b3ce6
Ewan Keith <ewan.keith@mudano.com>
2020-02-11 15:48:51 +0000

    added an initial test to run production app script to see if we can keep env vars for config injection, but source the values from ssm rather than a .env file

--- run-production-application.sh
+++ run-production-application.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+get_from_ssm () {
+  TEMP="$(aws ssm get-parameter --name $1 --query 'Parameter.Value')"
+  TEMP=$(eval echo $TEMP)
+  echo $TEMP
+}
+
+
+export OPEN_APS_POSTGRES_HOST=$(get_from_ssm openaps-postgres-host)
+
+
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml up --build \
   --force-recreate -d
 
