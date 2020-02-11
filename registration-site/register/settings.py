ccbaff3a7d007b99f0631ac1f2560972a61bd3d1
Ewan Keith <ewan.keith@mudano.com>
2020-02-11 16:16:39 +0000

    Revert "added a second ssm test in regsitration app to be sure it works before carrying out a big code change"
    This reverts commit fda53b6b5fd0f2b74e5ebecfbf7b188e86d87d41.

--- registration-site/register/settings.py
+++ registration-site/register/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'register.wsgi.application'
 # get postgres configuration from the environment
 POSTGRES_HOST = os.environ['POSTGRES_HOST']
 POSTGRES_PORT = os.environ['POSTGRES_PORT']
-POSTGRES_DB_NAME = ssm.get_parameter(Name = 'aurora-db-name')['Parameter']['Value']
+POSTGRES_DB_NAME = ssm.get_parameter(Name = 'aurora-db-name')['Parameter']['Value'] # os.environ['POSTGRES_DB']
 POSTGRES_USER = os.environ['POSTGRES_USER']
 POSTGRES_PASS = os.environ['POSTGRES_PASSWORD']
 
@@ -81,7 +81,7 @@ APPLICATION_PORT = os.getenv('APP_PORT')
 
 OPENHUMANS_OH_BASE_URL = 'https://www.openhumans.org'
 OPENHUMANS_PROJECT_ADDRESS = os.getenv('OPEN_HUMANS_PROJECT_ADDRESS')
-OPENHUMANS_CLIENT_ID = ssm.get_parameter(Name = 'open-humans-client-id')['Parameter']['Value'] # os.getenv('OPEN_HUMANS_CLIENT_ID')
+OPENHUMANS_CLIENT_ID = os.getenv('OPEN_HUMANS_CLIENT_ID')
 OPENHUMANS_CLIENT_SECRET = os.getenv('OPEN_HUMANS_CLIENT_SECRET')
 
 LOGIN_REDIRECT_URL = '/'
