776b4abbf612be34d0989927b4005e1d16f2bd74
Ubuntu <ubuntu@ip-172-31-41-29.eu-west-1.compute.internal>
2020-02-11 16:27:06 +0000

    updated sto production script so it doesnt print warnings that db host env var could not be found

--- stop-production.sh
+++ stop-production.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+
+get_from_ssm () {
+  TEMP="$(aws ssm get-parameter --name $1 --query 'Parameter.Value')"
+  TEMP=$(eval echo $TEMP)
+  echo $TEMP
+}
+
+export OPEN_APS_POSTGRES_HOST=$(get_from_ssm openaps-postgres-host)
+
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml down
 
 volume=$(docker volume inspect --format '{{json .Mountpoint}}' open-aps-streaming_open-aps-postgres-data)
 
 echo "\nThe Nightscout-Open Humans data solution has now been stopped." 
-echo " - Postgres data is still stored at $volume"
-echo " - You can start it again by calling ./run-production-application.sh"
-
 echo ""
 
