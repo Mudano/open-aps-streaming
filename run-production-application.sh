8602c189f772475830e3e1c8393bb827f4f5bfb4
Ewan Keith <ewan.keith@mudano.com>
2020-02-13 09:42:24 +0000

    added env var fetching in run-prod script for the nightscout ingester

--- run-production-application.sh
+++ run-production-application.sh
@@ -18,12 +18,13 @@ export OPEN_HUMANS_CLIENT_ID=$(get_from_ssm open-humans-client-id)
 export OPEN_HUMANS_PROJECT_ADDRESS=$(get_from_ssm open-humans-project-address)
 export OPEN_HUMANS_CLIENT_SECRET=$(get_from_ssm open-humans-client-secret)
 export REGISTER_APP_DJANGO_SECRET_KEY=$(get_from_ssm openaps-register-django-key)
+export OPEN_APS_POSTGRES_NIGHTSCOUT_USER=$(get_from_ssm postgres-nightscout-user)
+export OPEN_APS_NIGHTSCOUT_PASSWORD=$(get_from_ssm postgres-nightscout-password)
+export OPEN_APS_INGEST_INTERVAL_HOURS=$(get_from_ssm openaps-ingest-interval)
 
 
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml up --build \
   --force-recreate -d
-
-volume=$(docker volume inspect --format '{{json .Mountpoint}}' open-aps-streaming_open-aps-postgres-data)
 
 echo "\nThe Nightscout-Open Humans data solution is now running on this machine."
 echo " - service logs can be followed by calling ./attach-production-logs.sh"
