8d6873fdb6d4258884e47cb849c20b063a579dd1
Ewan Keith <ewan.keith@mudano.com>
2020-02-13 11:16:04 +0000

    added the rest of teh required env var fetched to the run prod script, env file should no longer be needed

--- run-production-application.sh
+++ run-production-application.sh
@@ -21,6 +21,20 @@ export REGISTER_APP_DJANGO_SECRET_KEY=$(get_from_ssm openaps-register-django-key
 export OPEN_APS_POSTGRES_NIGHTSCOUT_USER=$(get_from_ssm postgres-nightscout-user)
 export OPEN_APS_NIGHTSCOUT_PASSWORD=$(get_from_ssm postgres-nightscout-password)
 export OPEN_APS_INGEST_INTERVAL_HOURS=$(get_from_ssm openaps-ingest-interval)
+export OPEN_APS_POSTGRES_EXT_OPENAPS_APP_PASSWORD=$(get_from_ssm postgres-openaps-app-password)
+export OPEN_APS_DOWNLOADER_EMAIL=$(get_from_ssm openaps-downloader-app-email)
+export OPEN_APS_DOWNLOADER_EMAIL_PASS=$(get_from_ssm openaps-downloader-app-email-password)
+export OPEN_APS_DOWNLOADER_SECRET_KEY=$(get_from_ssm openaps-downloader-secret-key)
+export OPEN_APS_DOWNLOADER_SLACK_KEY=$(get_from_ssm openaps-downloader-slack-key)
+export OPEN_APS_DOWNLOADER_METABASE_KEY=$(get_from_ssm openaps-downloader-metabase-key)
+export OPEN_APS_DOWNLOADER_METABASE_URL=$(get_from_ssm openaps-metabase-url)
+export OPEN_APS_DOWNLOADER_ADMIN_EMAIL=$(get_from_ssm openaps-downloader-admin-email)
+export OPEN_APS_DOWNLOADER_PUBLIC_URL=$(get_from_ssm openaps-downloader-url)
+export OPEN_APS_POSTGRES_METABASE_PASSWORD=$(get_from_ssm open-aps-metabase-password)
+export OPEN_APS_POSTGRES_INGESTOR_PASSWORD=$(get_from_ssm postgres-ingestor-password)
+export OPEN_APS_ETL_INTERVAL_HOURS=$(get_from_ssm openaps-etl-interval)
+export OPEN_APS_OPENAPS_DEMOGRAPHICS_URL=$(get_from_ssm openaps-openaps-demographics-url)
+export OPEN_APS_NIGHTSCOUT_DEMOGRAPHICS_URL=$(get_from_ssm openaps-nightscout-demographics-url)
 
 
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml up --build \
