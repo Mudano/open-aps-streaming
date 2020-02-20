d0cb295a18aad2ff4c6e604a346d23e603d811ed
Ewan Keith <ewan.keith@mudano.com>
2020-02-20 10:08:38 +0000

    removed unnecesary commands from the stop-production script

--- stop-production.sh
+++ stop-production.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-get_from_ssm () {
-  TEMP="$(aws ssm get-parameter --name $1 --query 'Parameter.Value')"
-  TEMP=$(eval echo $TEMP)
-  echo $TEMP
-}
-
-export OPEN_APS_POSTGRES_HOST=$(get_from_ssm openaps-postgres-host)
-
 docker-compose -f docker-compose.yml -f prod.docker-compose.yml down
-
-volume=$(docker volume inspect --format '{{json .Mountpoint}}' open-aps-streaming_open-aps-postgres-data)
 
 echo "\nThe Nightscout-Open Humans data solution has now been stopped." 
 echo ""
